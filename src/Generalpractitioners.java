e1409e46a8434944ff13221c67df05afa088c5b7
PaiDaXingIII <162035030+PaiDaXingIII@users.noreply.github.com>
2024-11-18 19:18:32 +0800

    Changed the type of doctor I like, and completed the content of both classes.

---
+++ src/Generalpractitioners.java
@@ -0,0 +1,27 @@
+public class Generalpractitioners extends HealthProfessional {
+
+    private String department;
+    public Generalpractitioners() {
+        super();
+    }
+
+    public Generalpractitioners(int ID, String name, String type, String department) {
+        super(ID, name, type);
+        this.department = department;
+    }
+
+    @Override
+    public void printAllVariables() {
+        super.printAllVariables();
+        System.out.println("Department: " + department);
+        System.out.println("Health Professional Type: Other Health Professional");
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
